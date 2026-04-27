e6240325b83bc84e9ca7d2f406556f3498be2c66
jacksonmarshall-droid <jackson.marshall@codedbykids.org>
2026-04-27 22:57:58 +0000

    pt.2

---
+++ app.js
@@ -0,0 +1,28 @@
+let coin="heads"
+// if (coin === "heads") {
+//     console.log("toss is heads")
+// }
+
+// if (coin === "tails") {
+//     console.log("toss is tails")
+// }
+
+if (coin === "heads") {
+    console.log("toss is heads")
+}else{ 
+    console.log("toss is tails")
+} 
+let binary=1
+if (binary === 1){
+    console.log("binary=1")
+}else{
+    console.log("binary=0")
+}
+let color="green" 
+if (color==="green"){
+    console.log("green=go")
+}else if (color==="yellow"){
+  console.log("yellow=slow")
+}else{
+    console.log("red=stop")
+}
